ecaf04d2028f4674f446d35ccf322d8a11f73b8c
friedrich420 <kelpidorou@yahoo.com>
2015-03-10 18:12:55 +0200

    Revert "intelli/msm_thermal (thanks to faux123)"
    This reverts commit a00f813c1207836b99ba43e8af3247ae908c6154.

--- include/linux/msm_thermal.h
+++ include/linux/msm_thermal.h
@@ -19,15 +19,8 @@ struct msm_thermal_data {
 	uint32_t poll_ms;
 	int32_t limit_temp_degC;
 	int32_t temp_hysteresis_degC;
-<<<<<<< HEAD
 	uint32_t bootup_freq_step;
 	uint32_t bootup_freq_control_mask;
-=======
-	uint32_t freq_step;
-#ifdef CONFIG_INTELLI_THERMAL
-	uint32_t freq_control_mask;
-#endif
->>>>>>> 86572ca... intelli/msm_thermal (thanks to faux123)
 	int32_t core_limit_temp_degC;
 	int32_t core_temp_hysteresis_degC;
 	int32_t hotplug_temp_degC;
@@ -46,7 +39,7 @@ struct msm_thermal_data {
 	int32_t therm_reset_temp_degC;
 };
 
-#if defined(CONFIG_THERMAL_MONITOR) || defined(CONFIG_INTELLI_THERMAL)
+#ifdef CONFIG_THERMAL_MONITOR
 extern int msm_thermal_init(struct msm_thermal_data *pdata);
 extern int msm_thermal_device_init(void);
 extern int msm_thermal_set_frequency(uint32_t cpu, uint32_t freq,
